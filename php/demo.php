16138273ddfe530567b1f50c4b5fc2dcf1bc19aa
nicolachoquet06250 <nicolachoquet06250@gmail.com>
2018-06-07 22:38:27 +0200

    test avec une 2em url en local avec ormframework V2

--- php/demo.php
+++ php/demo.php
@@ -7,8 +7,13 @@
 	require_once 'lib/Autoload.php';
 	Auto::load();
 
-	if($request = OrmframeworkRequest::instence('meteo-ormf.nicolaschoquet.fr/dev')) {
-		$query = $request->icons()->get();
+	$url_server_v1 = 'meteo-ormf.nicolaschoquet.fr/dev';
+	$url_server_v2 = 'localhost/ormframeworkV2/rest';
+
+	$url_server = $url_server_v2;
+
+	if( $request = OrmframeworkRequest::instence($url_server) ) {
+		$query = $request->HelloWorld()->test('var1=toto', 'var2=lol');
 		$result = $query->go();
 
 		var_dump($result);
